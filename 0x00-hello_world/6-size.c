fcc798bc79229b94445cd5439eff482feaa6f92c
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-07 23:58:27 +0300

    finish

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main - The program starts
+*
+*Description : This program prints somthing 
+*
+*Return : Always main returns 0
+*/
+
+int main(void)
+{
+char c 
+int i
+long int li
+long long int lli
+
+printf("Size of a char: ", sizof(c));
+printf("Size of an int: ", sizof(i));
+printf("Size of a long int: ", sizof(li));
+printf("Size of a long long int ", size(lli));
+
+return (0);
+}
