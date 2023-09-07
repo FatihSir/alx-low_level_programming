c1205a0bee30d6a7c93792dcac6733a24a67de57
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-08 00:10:47 +0300

    gfchvjbknl

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
-* main : The program starts
+* main: The program starts
 *
-* Description : This program uses sizeof function 
+* Description: This program uses sizeof function 
 *
-* return : Always main returns 0
+* return: Always main returns 0
 */
 
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizof(char));
-printf("Size of an int: %lu byte(s)\n", sizof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", size(long long int));
-printf("Size of a float: %lu byte(s)\n", size(float));
-
-return (0);
+	printf("Size of a char: %lu byte(s)\n", sizof(char));
+	printf("Size of an int: %lu byte(s)\n", sizof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", size(long long int));
+	printf("Size of a float: %lu byte(s)\n", size(float));
+	return (0);
 }
