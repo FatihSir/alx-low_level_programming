83f551d053335e4885838d8cc900ed7ccf7b3dc1
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-07 23:46:32 +0300

    5-done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - The starting point of the program
+*
+*Description: This program print specific peice of words
+*
+*Return: Always returns 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
