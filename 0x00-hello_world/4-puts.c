9f3d65740194d74fb51282e58b81eda8a1cfcc0c
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-07 23:36:43 +0300

    jdfvpo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+* main - the program starts
+*
+* Description:This is discription
+*
+* Return: always 0
+*/
 
-int main (void)
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-
-return 0;
+        puts("Programming is like building a multilingual puzzle");
+        return (0);
 }
