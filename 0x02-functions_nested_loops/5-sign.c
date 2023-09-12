bdd95595adefb4cf8579ed97917b19cf7bd9fb22
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-12 14:43:29 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,19 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n > 1)
 	{
-		_putchar(n);
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(n);
+		_putchar(48);
 		return (0);
 	}
 	else
 	{
-		_putchar(n);
+		_putchar(45);
 		return (-1);
 	}
 	return (0);
