f7e0db4e0bb23662ee300e6edee1558420106363
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-12 14:37:16 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_sign - program entry
+*
+* Describtion: print_sign
+* @n: is the number to be checked
+* Return: The program always returns 0
+*
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar(n);
+		return (1);
+	if (n == 0)
+		_putchar(n);
+		return (0);
+	if
+		_putchar(n);
+		return (-1);
+	return (0);
+}
