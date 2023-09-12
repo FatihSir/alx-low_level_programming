c497dd75428705814e365d335cacc29432c4a152
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-12 13:46:03 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - program entry
+*
+* Describtion: The programs prints the lowercase
+*
+* Return: The program always returns 0
+*
+*/
+
+int main(void)
+{	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}
