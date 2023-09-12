5bebbe070c7477338c43c776cc4e2a39ca01ae14
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-12 13:34:09 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - where the program starts
+*
+* Describtion: This progrm prints _putchar followed by newline
+*
+* Return: This program always returns 0
+*
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
