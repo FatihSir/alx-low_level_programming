c41048dc76f36b9027bed4392f4fc13dd22e1745
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-12 15:25:14 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _add - program entry
+*
+* Describtion: The program adds 2 numbers
+*
+* @a: first umber
+* @b: second number
+*
+* Return: The program always returns the summation of 2 numbers
+*/
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a +  b;
+	return (sum);
+}
