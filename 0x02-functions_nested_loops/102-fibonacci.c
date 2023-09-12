b5c954716383e486bab786dc1b677bd05c9b2c73
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-12 16:11:59 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* main - program starts
+*
+* Describtion: The program checks if it is a  letter
+*
+* Return: The program always returns 0 if it is a letter
+*/
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
