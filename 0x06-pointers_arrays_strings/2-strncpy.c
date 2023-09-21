529aa0b97890dcfaa81f9f816f8a2d944b0e1191
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-21 13:55:32 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,16 +15,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0';)
-	{
-		i++;
-	}
+
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
-		i++;
+		dest[j] = src[j];
 	}
-	if (srj[j] != '\0')
-		dest[i] = '\0';
+	if (j < n)
+		dest[j] = '\0';
 	return (dest);
 }
