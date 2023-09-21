8a2d683b3240b8e944849580e722810ef6bd2519
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-21 14:45:08 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,6 +13,21 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	i = s1 - s2;
-	return (i);
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; ++i)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+
+	if (s1[i] != '\0')
+	{
+		return (s1[i]);
+	}
+	else if (s2[i] != '\0')
+	{
+		return (-s2[i]);
+	}
+	return (0);
 }
