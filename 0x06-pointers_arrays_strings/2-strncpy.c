a62eca534c36953952aeb99e4f179f7ae80d7e46
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-21 13:19:57 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strncpy - function copies strings from source to destination but specifies
+* the maximum number of characters to be copied
+*
+* @dest: the first string
+* @src: the second string
+* @n: the maximum number of characters
+*
+* Return: returns pointer points to the strings after copied the source
+*
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0';)
+	{
+		i++;
+	}
+	for (j = 0; j < n | src[j] == '\0'; j++)
+	{
+		if (src[j] == '\0')
+			break;
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
