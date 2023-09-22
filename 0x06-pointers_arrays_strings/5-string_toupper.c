1fadf69c1b0b1635759a6e657bfebf5d8098ef96
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-22 13:58:30 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* string_toupper - function converts lowercase
+* characters in a string to uppercase
+*
+* @str: The string array
+*
+* Return: returns strings
+*
+*/
+char *string_toupper(char *str)
+{
+	for (int i = 0; str[i] != '\0'; ++i)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+	}
+	return (str);
+}
