c8c32a7aaf198cb61548bd4190a481533377b748
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-25 11:45:08 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,10 +14,10 @@ unsigned int _strspn(char *s, char *accept)
 	int i;
 	int j;
 
-	for (i = 0; accept[i] != '\0'; ++i)
-		for (j = 0; s[j] != '\0'; ++j)
+	for (i = 0; s[i] != '\0'; ++i)
+		for (j = 0; accept[j] != '\0'; ++j)
 		{
-			if (accept[i] == s[j])
+			if (s[i] == accept[j])
 				counter =  counter + 1;
 
 		}
