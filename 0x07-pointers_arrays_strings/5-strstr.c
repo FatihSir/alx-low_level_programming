2340f3a3c6f6aa4a86efc44f0bbabc5cf4517f84
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-25 13:37:12 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* _strstr - a function that locates a substring.
+*
+* @needle: the string to be looked for
+* @haystack: the string to be looked within
+*
+* Return: a pointer
+*
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *ptr;
+
+	for (i = 0; needle[i] != '\0'; ++i)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; haystack[j] != '\0'; ++j)
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					break;
+				}
+			}
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	return ('\0');
+}
