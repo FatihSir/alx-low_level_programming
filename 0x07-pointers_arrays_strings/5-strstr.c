34e74bcb6d7fe37254a68ae2d07e67ab54ebcc85
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-25 13:50:54 -0700

    Fix Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,11 +12,11 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; needle[i] != '\0'; ++i)
+	for (i = 0; haystack[i] != '\0'; ++i)
 	{
 		if (haystack[i] == needle[0])
 		{
-			for (j = 0; haystack[j] != '\0'; ++j)
+			for (j = 0; needle[j] != '\0'; ++j)
 			{
 				if (haystack[i + j] != needle[j])
 				{
