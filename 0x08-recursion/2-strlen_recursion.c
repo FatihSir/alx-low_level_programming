81765e228f60923ab8e5425c276161ef735d3b94
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-09-28 14:11:08 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,16 +8,12 @@
 */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
-
 	if (*s == '\0')
 	{
-		return (length);
+		return (0);
 	}
 	else
 	{
-		length = length + 1;
-		_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return (length);
 }
