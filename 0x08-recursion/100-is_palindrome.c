2e76095ad791a59cc1629f9288cb34fb2eb46423
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-01 13:18:31 -0700

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+* checker2 - function that helps the main function
+*
+* @a: the string to be compared
+* @c: counts length
+*
+* Return: return 0 or 1
+*/
+int checker2(char *a, int c)
+{
+	if (*a == 0)
+		return (c - 1);
+	return (checker2(a + 1, c + 1));
+}
+/**
+ * checker1 - function that helps the main function
+ * @a: the string to be compared
+ * @c: the length
+ *
+ * Return: returns 0 or 1
+ */
+
+int checker1(char *a, int c)
+{
+	if (*a != *(a + c))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (checker2(a + 1, c - 2));
+}
+/**
+* is_palindrome - a function that returns 1 if a string is
+* a palindrome and 0 if not
+*
+* @s: the string to be checked
+*
+* Return: returns 1 if a string is a palindrome and 0 if not
+*/
+int is_palindrome(char *s)
+{
+	int c;
+
+	c = checker1(s, 0);
+	return (checker2(s, c));
+}
