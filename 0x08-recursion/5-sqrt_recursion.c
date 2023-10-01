3ea7729f7c0ae8cf325f8708a85d66eb8fa462ec
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-01 12:09:55 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,11 +3,29 @@
 * _sqrt_recursion - a function that returns the natural square root of a number
 *
 * @n: number to be squaredi root
+* @a: integer to iterate util n
+* Return: retuns the squared root of n
+*/
+int checker(int n, int a)
+{
+	if (a * a == n)
+		return (a);
+	else if (a == n)
+		return (-1);
+	else
+		return (checker(n, a + 1));
+}
+/**
+* _sqrt_recursion - a function that returns the natural square root of a number
+*
+* @n: number to be squaredi root
 *
 * Return: retuns the squared root of n
 */
 int _sqrt_recursion(int n)
 {
-
+	if (n == 0)
+		return (0);
+	else
+		return (checker(n, 1));
 }
-
