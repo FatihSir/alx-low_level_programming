3b2537d1cd4653394695b766172f022ce4794417
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-04 14:44:51 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -22,7 +22,7 @@ char *_strdup(char *str)
 	{
 		n = n + 1;
 	}
-	ptr = malloc((n + 1) * sizeof(char));
+	ptr = malloc(n * sizeof(char) + 1);
 	if (ptr == 0)
 		return (0);
 	for (i = 0; i < n; ++i)
