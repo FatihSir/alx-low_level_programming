2813f934958214ae7255fee53720a6c9cdd4a732
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-04 15:27:00 -0700

    Fix Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,9 +24,9 @@ char *str_concat(char *s1, char *s2)
 	{
 		size_2++;
 	}
-	if (s1 == '\0')
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == '\0')
+	if (s2 == NULL)
 		s2 = "";
 	ptr = malloc(size_1 + size_2 + 1);
 	if (ptr == 0)
