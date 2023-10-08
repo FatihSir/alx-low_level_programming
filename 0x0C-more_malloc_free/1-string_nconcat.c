4e9adce1f49972c4a903a7658c9e20d4f1963f1e
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-08 07:58:16 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,28 +4,28 @@
 *
 * @s1: the first string
 * @s2: the second string
-* @s1_length: the length of s1
-* @s2_length: the length of s2
+* @s1_len: the length of s1
+* @s2_len: the length of s2
 *
 * Return: returns a pointer that points to a newly allocated space in memory
 * which contains s1, followed by the first n bytes of s2, and null terminated
 *
 */
-char *conc_1(unsigned int s1_length, unsigned int s2_length, char *s1, char *s2)
+char *conc_1(unsigned int s1_len, unsigned int s2_len, char *s1, char *s2)
 {
 	int i, j;
 	char *conc;
 
-	conc = malloc((s1_length + s2_length + 1) * sizeof(char));
+	conc = malloc((s1_len + s2_len + 1) * sizeof(char));
 	if (conc == NULL)
 	{
 		return (0);
 	}
-	for (i = 0; i < s1_length; ++i)
+	for (i = 0; i < s1_len; ++i)
 	{
 		conc[i] = s1[i];
 	}
-	for (j = 0; j < s2_length; ++j)
+	for (j = 0; j < s2_len; ++j)
 	{
 		conc[i + j] = s2[j];
 	}
@@ -37,24 +37,24 @@ char *conc_1(unsigned int s1_length, unsigned int s2_length, char *s1, char *s2)
 *
 * @s1: the first string
 * @s2: the second string
-* @s1_length: the length of s1
+* @s1_len: the length of s1
 * @n: the length of s2 to be concatenated
 *
 * Return: returns a pointer that points to a newly allocated space in memory
 * which contains s1, followed by the first n bytes of s2, and null terminated
 *
 */
-char *conc_2(unsigned int s1_length, unsigned int n, char *s1, char *s2)
+char *conc_2(unsigned int s1_len, unsigned int n, char *s1, char *s2)
 {
 	int i, j;
 	char *conc;
 
-	conc = malloc((s1_length + n + 1) * sizeof(char));
+	conc = malloc((s1_len + n + 1) * sizeof(char));
 	if (conc == NULL)
 	{
 		return (0);
 	}
-	for (i = 0; i < s1_length; ++i)
+	for (i = 0; i < s1_len; ++i)
 	{
 		conc[i] = s1[i];
 	}
@@ -78,36 +78,44 @@ char *conc_2(unsigned int s1_length, unsigned int n, char *s1, char *s2)
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_length;
-	unsigned int s2_length;
+	unsigned int s1_len;
+	unsigned int s2_len;
 	char *conc;
 	int i;
+	int l;
+	int m;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	s1_length = strlen(s1);
-	s2_length = strlen(s2);
-
-	if (n >= s2_length)
+	for (l = 0; s1[l] != '\0'; ++l)
 	{
-		conc = conc_1(s1_length, s2_length, s1, s2);
+		s1_len = s1_len + 1;
+	}
+	for (m = 0; s2[m] != '\0'; ++m)
+	{
+		s2_len = s2_len + 1;
+	}
+
+	if (n >= s2_len)
+	{
+		conc = conc_1(s1_len, s2_len, s1, s2);
 		return (conc);
 	}
 	else if (s2[0] == '\0')
 	{
-		conc = malloc((sizeof(char) * s1_length) + 1);
+		conc = malloc((sizeof(char) * s1_len) + 1);
 		if (conc == NULL)
 			return (0);
-		for (i = 0; i < s1_length; ++i)
+		for (i = 0; i < s1_len; ++i)
 		{
 			conc[i] = s1[i];
 		}
-		conc[s1_length] = '\0';
+		conc[s1_len] = '\0';
 		return (conc);
 	}
-	conc = conc_2(s1_length, n, s1, s2);
+	conc = conc_2(s1_len, n, s1, s2);
 	return (conc);
 }
