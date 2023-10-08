30c617464cc5d0eb6414b832ff65eeb6cbe29a1b
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-08 10:19:43 -0700

    Task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,91 @@
+#include "main.h"
+/**
+* _realloc_1 - a function that allocates a memory block using malloc and free
+*
+* @new_size: is the new size, in bytes of the new memory block
+*
+* Return: pointer to the new allocated memory with a copy of the old
+* allocated memory
+*/
+void *_realloc_1(unsigned int new_size)
+{
+	void *new_ptr;
+
+	new_ptr = malloc(new_size * sizeof(unsigned int));
+	if (new_ptr == NULL)
+		return (0);
+	return (new_ptr);
+}
+/**
+* _realloc_2 - a function that allocates a memory block using malloc and free
+*
+* @ptr: is a pointer to the memory previously allocated with a call to malloc
+*
+* Return: pointer to the new allocated memory with a copy of the old
+* allocated memory
+*/
+void *_realloc_2(void *ptr)
+{
+	free(ptr);
+	return (0);
+}
+/**
+* _realloc_3 - a function that allocates a memory block using malloc and free
+*
+* @ptr: is a pointer to the memory previously allocated with a call to malloc
+* @old_size: is the size, in bytes, of the allocated space for ptr
+* @new_size: is the new size, in bytes of the new memory block
+*
+* Return: pointer to the new allocated memory with a copy of the old
+* allocated memory
+*/
+void *_realloc_3(void *ptr, unsigned int new_size, unsigned int old_size)
+{
+	unsigned int *new_ptr;
+	unsigned int *ptr_1 = ptr;
+	unsigned int i;
+
+	new_ptr = malloc(new_size * sizeof(unsigned int));
+	if (new_ptr == NULL)
+		return (0);
+	for (i = 0; i <= old_size; ++i)
+	{
+		new_ptr[i] = ptr_1[i];
+	}
+	free(ptr);
+	return (new_ptr);
+}
+/**
+* _realloc - a function that reallocates a memory block using malloc and free
+*
+* @ptr: is a pointer to the memory previously allocated with a call to malloc
+* @old_size: is the size, in bytes, of the allocated space for ptr
+* @new_size: is the new size, in bytes of the new memory block
+*
+* Return: pointer to the new allocated memory with a copy of the old
+* allocated memory
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		new_ptr = _realloc_1(new_size);
+		return (new_ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		new_ptr = _realloc_2(ptr);
+		return (0);
+	}
+	if (new_size > old_size)
+	{
+		new_ptr = _realloc_3(ptr, new_size, old_size);
+		return (new_ptr);
+	}
+	return (new_ptr);
+
+}
