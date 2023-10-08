8d685c611b4d6889516df6fcaa129d25619166ab
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-08 08:50:16 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,13 +14,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (0);
-	ptr = malloc((nmemb * size) + 1);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (0);
 	for (i = 0; i < (nmemb * size); ++i)
 	{
 		ptr[i] = 0;
 	}
-	ptr[i + 1] = '\0';
 	return (ptr);
 }
