4ec571a1def5e5b29fa4ba7da9bf6fb0cc749f4f
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-09 05:56:07 -0700

    Task 6

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -16,7 +16,7 @@ int _isdigit(char *num_1, char *num_2)
 		if (!isdigit(num_1[i]))
 		{
 			printf("Error\n");
-			exit(3);
+			exit(98);
 		}
 	}
 	for (i = 0; num_2[i] != '\0'; ++i)
@@ -24,7 +24,7 @@ int _isdigit(char *num_1, char *num_2)
 		if (!isdigit(num_2[i]))
 		{
 			printf("Error\n");
-			exit(3);
+			exit(98);
 		}
 	}
 	return (0);
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit(3);
+		exit(98);
 	}
 
 	num_1 = argv[1];
