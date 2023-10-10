035abafed729799b8f1fdb7a20e4a19aa5dd0786
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-10 15:17:49 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+/**
+* struct dog - new struct
+*
+* @name: name
+* @age: age
+* @owner: owner
+*
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
