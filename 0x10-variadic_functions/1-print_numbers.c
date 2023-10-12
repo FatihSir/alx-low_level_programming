169ad6b1570260620efe44a6ac5244011aee8083
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-12 13:10:02 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,7 +10,8 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	int i, num;
+	unsigned int i;
+	int num = 0;
 
 	va_start(args, n);
 	for (i = 0; i < n; ++i)
