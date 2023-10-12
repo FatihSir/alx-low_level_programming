cb56b3def9739b34b344f1f32bfa43f8629b5296
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-10-12 16:04:36 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,59 @@
 #include "variadic_functions.h"
 /**
+* print_int - a function that prints integer
+*
+* @sep: a pointer to the separator
+* @args: the values to be printed
+*
+* Return: returns nothing
+*/
+void print_int(char *sep, va_list args)
+{
+	printf("%s%d", sep, va_arg(args, int));
+}
+/**
+* print_float - a function that prints float
+*
+* @sep: a pointer to the separator
+* @args: the values to be printed
+*
+* Return: returns nothing
+*/
+void print_float(char *sep, va_list args)
+{
+	printf("%s%f", sep, va_arg(args, double));
+}
+/**
+* print_char - a function that prints char
+*
+* @sep: a pointer to the separator
+* @args: the values to be printed
+*
+* Return: returns nothing
+*/
+void print_char(char *sep, va_list args)
+{
+	printf("%s%c", sep, va_arg(args, int));
+}
+/**
+* print_string - a function that prints string
+*
+* @sep: a pointer to the separator
+* @args: the values to be printed
+*
+* Return: returns nothing
+*/
+void print_string(char *sep, va_list args)
+{
+	char *ptr = va_arg(args, char *);
+
+	switch ((int)(!ptr))
+		case 1:
+			ptr = "(nil)";
+	printf("%s%s", sep, ptr);
+}
+
+/**
 * print_all - a function that prints anything
 *
 * @format:  is a list of types of arguments passed to the function
@@ -8,29 +62,32 @@
 */
 void print_all(const char * const format, ...)
 {
+	token_datatype tok[] = {
+		{"c", print_char},
+		{"¡", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	int i = 0, j = 0;
+	char *sep = "";
 	va_list args;
-	int intger, i;
-	double floa;
 
 	va_start(args, format);
-	if (*format == 'i')
+	while (format && format[i])
 	{
-		while (i < 5)
+		j = 0;
+		while (tok[j].frmt)
 		{
-			intger = va_arg(args, int);
-			printf("%d,", intger);
+			if (format[i] == tok[j].frmt[0])
+			{
+				tok[j].ops(sep, args);
+				sep = ", ";
+			}
+			j++;
 		}
-		printf("\n");
-		va_end(args);
+		i++;
 	}
-	if (*format == 'f')
-	{
-		while (i < 5)
-		{
-			floa = va_arg(args, double);
-			printf("%f,", floa);
-		}
-		printf("\n");
-		va_end(args);
-	}
+	printf("\n");
+	va_end(args);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,20 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+/**
+* struct tokens - srtuct datatype
+*
+* @format: pointer to the format
+* @ops: function pointer to call a specific function based on format
+*/
+
+
+typedef struct tokens
+{
+	char *frmt;
+	void (*ops)(char *, va_list);
+} token_datatype;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
