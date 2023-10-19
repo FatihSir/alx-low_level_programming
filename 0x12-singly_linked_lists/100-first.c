f3435787e46c6b4390591eaf691770a20e4bc924
FatihSir <mohamedalfateh83@gmail.com>
2023-10-19 14:46:51 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+* startup_function - a function that prints something before the main executed
+*/
+void startup_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
