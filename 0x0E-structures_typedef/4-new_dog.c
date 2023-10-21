14b00fe59cba19eb321f814960c179515779c9ed
FatihSir <mohamedalfateh83@gmail.com>
2023-10-21 02:59:15 -0700

    Task 4 fixes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,29 +12,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	char *name_copy, *owner_copy;
-	
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-	}
+
 	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	if (new_dog == NULL || name == NULL || owner == NULL)
 	{
 		free(new_dog);
-		return (NULL);
+		return (0);
 	}
 	name_copy = strdup(name);
 	if (name_copy == NULL)
 	{
 		free(new_dog);
-		return (NULL);
+		return (0);
 	}
 	owner_copy = strdup(owner);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
 		free(new_dog);
-		return (NULL);
+		return (0);
 	}
 	new_dog->name = name_copy;
 	new_dog->age = age;
