f7474cd1dd6bf5b79d186ffd60cbc58a0d058f12
FatihSir <mohamedalfateh83@gmail.com>
2023-10-26 16:08:24 -0700

    Task 0 update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,21 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	 unsigned int result = 0;
+	unsigned int result = 0;
+	int i;
 
-	 if (b == NULL)
-		 return (0);
-	 for (int i = 0; b[i] != '\0'; ++i)
-	 {
-		 if (b[i] == '0' || b[i] == '1')
-		 {
-			 result = result * 2 + (b[i] - '0');
-		 }
-		 else
-		 {
-			 return (0);
-		 }
-	 }
-	 return (result);
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i] != '\0'; ++i)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			result = result * 2 + (b[i] - '0');
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (result);
 }
