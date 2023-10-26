91cb4a7116b533fd1657f27f0881ec4d3aaca2d9
FatihSir <mohamedalfateh83@gmail.com>
2023-10-26 16:01:56 -0700

    Task 0 fixes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,33 +10,20 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	char *ptr;
-	int i, j, len, ops = 0;
-	unsigned int b_toi = 0;
+	 unsigned int result = 0;
 
-	for (len = 0;b[len] != '\0'; ++len);
-	if (b == NULL)
-		return (0);
-	ptr = malloc((len + 1) * sizeof(char));
-	if (ptr == NULL)
-		return (0);
-	for (i = 0, j = len - 1; i < len && j >= 0; ++i, --j)
-	{
-		if (b[j] == '0' || b[j] == '1')
-		{
-			ptr[i] = b[j] - '0';
-		}
-		else
-		{
-			free(ptr);
-			return (0);
-		}
-	}
-	for (i = 0; i < len; ++i)
-	{
-		ops = (int)_pow(2, i) * ptr[i];
-		b_toi = b_toi + ops;
-	}
-	free(ptr);
-	return (b_toi);
+	 if (b == NULL)
+		 return (0);
+	 for (int i = 0; b[i] != '\0'; ++i)
+	 {
+		 if (b[i] == '0' || b[i] == '1')
+		 {
+			 result = result * 2 + (b[i] - '0');
+		 }
+		 else
+		 {
+			 return (0);
+		 }
+	 }
+	 return (result);
 }
