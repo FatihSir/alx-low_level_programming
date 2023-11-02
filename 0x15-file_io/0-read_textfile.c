281839a91eff93d684a5562a8a6e59aa781c6aed
FatihSir <mohamedalfateh83@gmail.com>
2023-11-02 09:18:30 -0700

    1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -40,7 +40,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		buffer[bytes_read] = '\0';
 		if ((ssize_t)letters > bytes_read)
 			fprintf(stderr, "%s", buffer);
-		if ((ssize_t)letters <= bytes_read)
+		else
 			printf("%s", buffer);
 	}
 	close(ptr);
