1fa29e3aad1af0a77c5cbd8e5f5d6d0178f71920
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-23 23:31:51 +0300

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+* hash_table_print -  a function that prints a hash table.
+*
+* @ht: the hash table
+*
+* Return: nothing
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current_item;
+	unsigned long int i = 0;
+	char *sep = "";
+	
+	if (ht == NULL || ht->array == NULL)
+		return;
+	putchar('{');
+	for (i = 0; i < ht->size; ++i)
+	{
+		current_item = ht->array[i];
+		while (current_item != NULL)
+		{
+			printf("%s'%s': '%s'", sep, current_item->key, current_item->value);
+			sep = ", ";
+			current_item = current_item->next;
+		}
+	}
+	puts("}\n");
+}
