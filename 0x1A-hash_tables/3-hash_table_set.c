3a54ce9cf6e486e6cf16949695ba75fce321d420
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-23 20:07:44 +0300

    Fixes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,32 +14,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *current_item, *item = NULL;
 
-	if (*key == '\0')
+	if (*key == '\0' || ht == NULL || key == NULL || value == NULL)
 		return (0);
-	index = key_index((const unsigned char *)key, 1024);
+	index = key_index((const unsigned char *)key, ht->size);
 	current_item = ht->array[index];
-	if (current_item == NULL)
+	item = create_item((const unsigned char *)key, value);
+	while (current_item)
 	{
-		item = create_item((const unsigned char *)key, value);
-		ht->array[index] = item;
-		return (1);
-	}
-	else
-	{
-		while (current_item)
+		if (strcmp(current_item->key, key) == 0)
 		{
-			if (strcmp(current_item->key, key) == 0)
-			{
-				free(current_item->value);
-				current_item->value = strdup(value);
-				if (current_item->value == NULL)
-					return (0);
-				return (1);
-			}
+			free(current_item->value);
+			current_item->value = strdup(value);
+			if (current_item->value == NULL)
+				return (0);
+			return (1);
 		}
 		current_item = current_item->next;
-		item->next = ht->array[index];
-		ht->array[index] = item;
-		return (1);
 	}
+	item->next = ht->array[index];
+	ht->array[index] = item;
+	return (1);
 }
