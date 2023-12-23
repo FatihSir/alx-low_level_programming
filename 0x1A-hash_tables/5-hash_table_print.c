e0429272afa0908b07f8cb41d029d1db2fa2c983
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-23 23:37:25 +0300

    Task 5 fixes

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *current_item;
 	unsigned long int i = 0;
 	char *sep = "";
-	
+
 	if (ht == NULL || ht->array == NULL)
 		return;
 	putchar('{');
