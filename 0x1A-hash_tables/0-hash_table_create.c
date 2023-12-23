3226c720ce2498038fee0cacdfdfbf2f5158ca27
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-23 15:33:42 +0300

    Task 1 fixes : items initialization

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,9 +10,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
+	unsigned long int i = 0;
 
-	table = (hash_table_t *)malloc(size * sizeof(hash_table_t));
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
+	table->size = size;
+	table->array = (hash_node_t **)malloc(table->size * sizeof(table->array));
+
+	for (i = 0; i < size; ++i)
+	{
+		table->array[i] = NULL;
+	}
 	return (table);
 }
