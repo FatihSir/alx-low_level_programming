b796dd14ae038612bfdb214e3c4b115732613d4e
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-31 19:13:52 +0300

    Task 8 some fixes

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,7 +19,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = iter->next;
-		iter->prev = NULL;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(iter);
 		return (1);
 	}
