4f88e907ff34ce1775542d38858f2a1c556a4705
FatihSir <140240594+FatihSir@users.noreply.github.com>
2023-12-31 18:08:21 +0300

    Task 7 even more fixes

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (iter->next == NULL)
 	{
-		return (adddnodeint_end(h, n));
+		return (add_dnodeint_end(h, n));
 	}
 	iter->next->prev = new;
 	new->next = iter->next;
